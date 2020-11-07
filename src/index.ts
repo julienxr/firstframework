5c65f1d128e80be421e42f6b44b2f84e66494c80
julienxr <julienxr@gmail.com>
2020-11-07 08:37:40 -0600

    Created User, Attributes, Eventing, and Sync Classes via composition delegations from User.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Rather be doing Scala, lol.")
+import {User} from './models/User';
+
+const user = new User({name: 'new record', age: 0})
+user.events.on( 'change', () => [
+    console.log( 'Change' )
+] );
+
+user.events.trigger( 'change' );
---
+++ src/models/Attributes.ts
@@ -0,0 +1,29 @@
+export class Attributes<T> {
+
+    constructor( private data: T){}
+
+    get<K extends keyof T>(key: K): T[K]{
+        return this.data[key];
+    }
+
+    set(update: T): void {
+        Object.assign(this.data, update);
+    }
+}
+
+
+
+/* Notes
+1. Made class a generic type of <T>
+2.  get<K extends keyof T> <-- Setting up generic constraint stating that K can only be a key of T --> (key: K): T[K]{
+        return this.data[key];
+    }
+3. Example: 
+const attrs = new Attributes<UserProps>( {
+    id: 5, 
+    age: 20,
+    name: 'Xavier'
+} )
+
+const name = attrs.get('name')
+*/
